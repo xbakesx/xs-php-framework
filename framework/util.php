c0b0ec08b4852e392063ea5ff4088553bb3b4df6
Pete Carr <petrogad@gmail>
2012-08-16 10:42:34 -0500

    removed OB
    removed output buffering; and how it's handled.

--- framework/util.php
+++ framework/util.php
@@ -4,3 +4,25 @@
  * This file is filled with php functions that every page will be loading.  
  * Careful what you put in here.
  */
+function divide($str, $sep, $returnFirst = false)
+{
+	$index = strpos($str, $sep);
+
+	if ($index !== false)
+	{
+		$ret = array(substr($str, 0, $index), substr($str, $index + strlen($sep)));
+	}
+	else
+	{
+		$ret = array($str, '');
+	}
+
+	if ($returnFirst === true)
+	{
+		return array_shift($ret);
+	}
+	else
+	{
+		return $ret;
+	}
+}
